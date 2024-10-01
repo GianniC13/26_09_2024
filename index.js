4403fe7d09e3d90ef8d53bfcee429dc19ea52372
GianniC13 <ion.craiu@aol.com>
2024-10-01 21:20:14 +0100

    add variables

---
+++ index.js
@@ -0,0 +1,16 @@
+console.log("Hello world");
+
+// variabila declarata si apoi atribuita valoare
+let userName;
+userName = "Monica";
+console.log(userName);
+userName = "_monica_";
+console.log(userName);
+
+// variabila declarata si initiata cu o valoare intr-o linie
+let email = "user@test.com";
+console.log(email);
+
+// constanta declarata cu valoare
+const pi = 3.14;
+// pi = 3.12; - interzisa atribuirea de valori unei constante
